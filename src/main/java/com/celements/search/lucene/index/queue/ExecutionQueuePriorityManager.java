d349bf0484edf703238c1095ae5c508631139202
Marc Sladek <marc.sladek@synventis.com>
2018-12-11 16:32:23 +0100

    implement LuceneIndexingPriorityQueue and ExecutionQueuePriorityManager

---
+++ src/main/java/com/celements/search/lucene/index/queue/ExecutionQueuePriorityManager.java
@@ -0,0 +1,37 @@
+package com.celements.search.lucene.index.queue;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.context.Execution;
+import org.xwiki.context.ExecutionContext;
+
+import com.google.common.base.Optional;
+
+@Component
+public class ExecutionQueuePriorityManager implements IndexQueuePriorityManager {
+
+  private static final String EXEC_CONTEXT_KEY = "lucene.index.queue.priority";
+
+  @Requirement
+  private Execution exec;
+
+  @Override
+  public Optional<IndexQueuePriority> get() {
+    IndexQueuePriority priority = null;
+    Object property = getExecContext().getProperty(EXEC_CONTEXT_KEY);
+    if (property instanceof IndexQueuePriority) {
+      priority = (IndexQueuePriority) property;
+    }
+    return Optional.fromNullable(priority);
+  }
+
+  @Override
+  public void put(IndexQueuePriority priority) {
+    getExecContext().setProperty(EXEC_CONTEXT_KEY, priority);
+  }
+
+  private ExecutionContext getExecContext() {
+    return exec.getContext();
+  }
+
+}
